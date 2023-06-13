f2e274baee66defed5fd57a1614400851116c511
Leart Zuka <leart.zuka@web.de>
2023-06-14 01:50:51 +0200

    fix: change keybinds for harpoon

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -4,5 +4,5 @@ local ui = require("harpoon.ui")
 vim.keymap.set("n","<leader>a", mark.add_file)
 vim.keymap.set("n","<C-e>", ui.toggle_quick_menu)
 
-vim.keymap.set("n","<C-h>", function() ui.nav_next() end)
-vim.keymap.set("n","<C-t>", function() ui.nav_prev() end)
+vim.keymap.set("n","<C-n>", function() ui.nav_next() end)
+vim.keymap.set("n","<C-b>", function() ui.nav_prev() end)
