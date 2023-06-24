cb9b586b1c6470a95fd46d4faeaedb3a728708c1
Leart Zuka <leart.zuka@web.de>
2023-06-24 17:54:52 +0200

    feat: sum fish stuff

--- fish/config.fish
+++ fish/config.fish
@@ -1,23 +1,29 @@
 alias ls='ls --color=auto'
 alias ff='firefox'
-alias e="exit"
-alias cd..="cd .."
-alias black="~/.local/bin/black ."
-alias gg="git pull"
-alias gf="git fetch"
-alias gc="git checkout"
-alias gs="git status"
-alias cl="clear"
-alias activate="source .venv/bin/activate.fish"
-alias pytest="python -m pytest"
-alias sps="sudo pacman -S"
-alias spy="sudo pacman -Syu"
-alias emacs="devour emacsclient -c -a 'emacs'"
-alias bornagain=.~/born_again_build_dirs/bornagain/build/bin/bornagain
+alias e='exit'
+alias cd..='cd ..'
+alias black='~/.local/bin/black .'
+alias gg='git pull'
+alias gf='git fetch'
+alias gc='git checkout'
+alias gs='git status'
+alias lg='lazygit'
+alias cl='clear'
+alias activate='source .venv/bin/activate.fish'
+alias pytest='python -m pytest'
+alias music='ncmpcpp'
+alias p='pnpm'
+alias nv='nvim'
+
+function ggg
+  git add .
+  git commit -m $argv
+  git push
+end
 
 pyenv init - | source
 status --is-interactive; and pyenv virtualenv-init - | source
 starship init fish | source
-
+fnm env --use-on-cd | source
 function fish_greeting
 end
