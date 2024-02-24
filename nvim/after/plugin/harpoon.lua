1ef60b5895738babc558c700f1961eb2ccc40a2c
Leart Zuka <leart.zuka@web.de>
2024-02-24 02:57:59 +0100

    chore: delete nvim config

--- nvim/after/plugin/harpoon.lua
+++
@@ -1,9 +0,0 @@
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
-
-vim.keymap.set("n","<C-a>", mark.add_file)
-vim.keymap.set("n","<C-e>", ui.toggle_quick_menu)
-vim.keymap.set("n","<C-r>", mark.clear_all)
-
-vim.keymap.set("n","<C-n>", function() ui.nav_next() end)
-vim.keymap.set("n","<C-b>", function() ui.nav_prev() end)
--- nvim/lua/lz/init.lua
+++
@@ -1,2 +0,0 @@
-require("lz.remap")
-require("lz.set")
