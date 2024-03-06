d84c8015dda89e782dee0a68edcfe93bd7ca9194
Leart Zuka <leart.zuka@web.de>
2024-03-06 01:47:48 +0100

    feat: change colors of dashboard

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,7 +2,14 @@ return {
     "navarasu/onedark.nvim",
     init = function()
         require('onedark').setup {
-            style = 'deep'
+            style = 'deep',
+            highlights = {
+                DashboardHeader = { fg = '#91a3e3' },
+                DashboardKey = { fg = '#EA3C53' },
+                DashboardIcon = { fg = '#EA3C53' },
+                DashboardFooter = { fg = '#d8c8d5' },
+
+            }
         }
         require('onedark').load()
     end,
