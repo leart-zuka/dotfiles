4ee9c8b767c6fa76daf4e15fc7579e9844c94148
Leart Zuka <leart.zuka@web.de>
2024-04-09 19:05:07 +0200

    fix: include nvim-nio to remove debugger pluging bug

--- nvim/lua/plugins/debugger.lua
+++ nvim/lua/plugins/debugger.lua
@@ -4,6 +4,7 @@ return {
         "rcarriga/nvim-dap-ui",
         "theHamsta/nvim-dap-virtual-text",
         "mfussenegger/nvim-dap-python",
+        "nvim-neotest/nvim-nio",
     },
     config = function()
         local dap = require("dap")
