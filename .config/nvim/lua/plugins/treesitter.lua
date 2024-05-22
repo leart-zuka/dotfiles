dce5fcb2a7117b118acab41ec47541dba501d1c3
Leart Zuka <leart.zuka@web.de>
2024-05-22 19:39:50 +0200

    fix: disable treesitter for fish config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -41,7 +41,7 @@ return {
                 -- Using this option may slow down your editor, and you may see some duplicate highlights.
                 -- Instead of true it can also be a list of languages
                 -- additional_vim_regex_highlighting = false,
-                disable = { "latex" },
+                disable = { "latex", "fish" },
                 additional_vim_regex_highlighting = { "latex", "markdown" },
             },
         }
