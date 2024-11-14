d123cebac41552750355bed92c3f2fb6320ac9af
Leart Zuka <leart.zuka@web.de>
2024-11-14 01:28:39 +0100

    feat: fish function for quickly firing up wiqi project

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -28,6 +28,33 @@ function ggg
   git push
 end
 
+# Function to move into wiqi dir, start tmux with 3 panes, one for the local server, one for code editor, and one for lazygit
+function wq
+    if test -d $HOME/wq/
+        cd $HOME/wq/
+
+        tmux has-session -t wiqi 2>/dev/null; or tmux new-session -d -s wiqi -n bun
+
+        # Configure bun pane
+        tmux send-keys -t wiqi:0 "bun dev" C-m
+
+        # Create neovim pane
+        tmux new-window -t wiqi -n neovim
+        tmux send-keys -t wiqi:neovim "n" C-m
+
+        # Create the "lazygit" window with no command running
+        tmux new-window -t wiqi -n lazygit
+
+        # move to neovim window in the end
+        tmux select-window -t wiqi:neovim
+
+        # Switch to the tmux session
+        tmux attach-session -t wiqi
+    else
+        echo "Please check if wiqi dir exists and is named wq"
+    end
+end
+
 # Function to move into notes dir, create notefile, and open it with nvim
 function on
     if test (count $argv) -eq 0  
