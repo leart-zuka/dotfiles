4d75b3a7c8b33358d6a18b24fa0dd3056062d2da
Leart Zuka <leart.zuka@web.de>
2025-02-07 23:08:31 +0100

    chore: update whichkey config

--- .config/nvim/lua/plugins/whichkey.lua
+++ .config/nvim/lua/plugins/whichkey.lua
@@ -10,48 +10,34 @@ return {
     },
     config = function()
         local wk = require("which-key")
-        wk.register({
-            ["<leader>"] = {
-                p = {
-                    name = "+file",
-                    f = { "Find File" },
-                    w = { "Find Word" },
-                    o = { "Old Files" },
-                },
-                l = {
-                    name = "+latex",
-                    l = { "Start/Stop Compilation" },
-                    v = { "View PDF" },
-                },
-                g = {
-                    name = "+git",
-                    l = { "Open LazyGit" },
-                    c = { "List commits with diff" },
-                    b = { "List all branches with log preview" },
-                    s = { "List current changes" },
-                },
-                f = {
-                    name = "+filebrowser",
-                    b = { "Open Filebrowser" },
-                },
-                d = {
-                    name = "+debugger",
-                    c = { "Continue" },
-                    o = { "Toggle debugger" },
-                    b = { "Toggle breakpoint" },
-                },
-                o = {
-                    name = "+obsidian",
-                    s = { "Search" },
-                    n = { "Insert Template" },
-                    p = {
-                        name = "+paste",
-                        i = "Image"
-                    }
-                }
-            },
-            ["<leader>w"] = { "<cmd>w<cr>", "Write File" },
-            ["<leader>q"] = { "<cmd>q<cr>", "Quit Session" },
-        })
+        wk.add(
+            {
+                { "<leader>d",   group = "debugger" },
+                { "<leader>db",  desc = "Toggle breakpoint" },
+                { "<leader>dc",  desc = "Continue" },
+                { "<leader>do",  desc = "Toggle debugger" },
+                { "<leader>f",   group = "filebrowser" },
+                { "<leader>fb",  desc = "Open Filebrowser" },
+                { "<leader>g",   group = "git" },
+                { "<leader>gb",  desc = "List all branches with log preview" },
+                { "<leader>gc",  desc = "List commits with diff" },
+                { "<leader>gl",  desc = "Open LazyGit" },
+                { "<leader>gs",  desc = "List current changes" },
+                { "<leader>l",   group = "latex" },
+                { "<leader>ll",  desc = "Start/Stop Compilation" },
+                { "<leader>lv",  desc = "View PDF" },
+                { "<leader>o",   group = "obsidian" },
+                { "<leader>on",  desc = "Insert Template" },
+                { "<leader>op",  group = "paste" },
+                { "<leader>opi", desc = "Image" },
+                { "<leader>os",  desc = "Search" },
+                { "<leader>p",   group = "file" },
+                { "<leader>pf",  desc = "Find File" },
+                { "<leader>po",  desc = "Old Files" },
+                { "<leader>pw",  desc = "Find Word" },
+                { "<leader>q",   "<cmd>q<cr>",                               desc = "Quit Session" },
+                { "<leader>w",   "<cmd>w<cr>",                               desc = "Write File" },
+            }
+        )
     end
 }
