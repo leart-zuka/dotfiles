0b535e4a0cf0bb3715fc51bd965a380c3f90bb02
Leart Zuka <leart.zuka@web.de>
2025-03-19 17:52:09 +0100

    fix: update markdown bs

--- .config/nvim/lua/plugins/markview.lua
+++ .config/nvim/lua/plugins/markview.lua
@@ -9,69 +9,12 @@ return {
         "nvim-tree/nvim-web-devicons"
     },
     config = function()
-        require("markview").setup({
-            highlight_groups = {
-                {
-                    group_name = "Heading1",
-                    value = { fg = "#1e1e2e", bg = "#91A3E3" }
-                },
-                {
-                    group_name = "Heading1Corner",
-                    value = { fg = "#91A3E3" }
-                },
-            },
-            headings = {
-                enable = true,
-                shift_width = 0,
-
-                -- heading_1 = {
-                --     style = "label",
-                --
-                --     padding_left = " ",
-                --     padding_right = " ",
-                --
-                --     corner_right = "",
-                --     corner_right_hl = "Heading1Corner",
-                --
-                --     hl = "Heading1"
-                -- }
-            },
-            Checkboxes = {
-                enable = true,
-
-                checked = {
-                    text = "✔", hl = "TabLineSel"
-                },
-                unchecked = {},
-                pending = {},
-                custom = {
-                    {
-                        match = "~",
-                        text = "◕",
-                        hl = "CheckboxProgress"
-                    }
-                }
-            },
-            code_blocks = {
-                style = "minimal",
-                icons = true,
-                position = nil,
-                min_width = 70,
-
-                pad_amount = 3,
-                pad_char = " ",
-
-                language_direction = "left",
-                language_names = { "python" },
-
-                hl = "CursorLine",
-
-                sign = true,
-                sign_hl = nil
-            }
-
-        });
-
         vim.cmd("Markview enableAll");
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "MarkviewAttach",
+            callback = function(event)
+                local data = event.data;
+            end
+        })
     end
 }
--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -5,7 +5,10 @@ return {
     dependencies = {
         -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
         "MunifTanjim/nui.nvim",
-        "rcarriga/nvim-notify"
+        -- OPTIONAL:
+        --   `nvim-notify` is only needed, if you want to use the notification view.
+        --   If not available, we use `mini` as the fallback
+        "rcarriga/nvim-notify",
     },
     config = function()
         require("noice").setup({
