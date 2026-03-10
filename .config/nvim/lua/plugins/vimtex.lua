5460d8fb76e2669b2181007f28ff3d1d6c8f91e7
Leart Zuka <leart.zuka@web.de>
2026-03-10 01:08:19 +0100

    feat: update on nvim vimtex

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -4,15 +4,22 @@ return {
     -- tag = "v2.15", -- uncomment to pin to a specific release
     init = function()
         -- VimTeX configuration goes here, e.g.
-        vim.g.vimtex_view_method = "zathura"
 
         vim.g.vimtex_quickfix_ignore_filters = {
             "Warning",
             "Package.*Warning",
+            "Missing character",
             "Underfull",
             "Overfull",
             "LaTeX Warning",
         }
+
+        vim.g.vimtex_quickfix_mode = 1
+        vim.g.vimtex_compiler_latexmk_engines = { ["_"] = "-lualatex -shell-escape" }
+        vim.g.vimtex_indent_on_ampersands = 0
+        vim.g.matchup_override_vimtex = 1
+        vim.g.vimtex_complete_enabled = 1
+        vim.g.vimtex_view_method = "zathura"
     end,
     config = function()
         vim.api.nvim_create_autocmd({ "FileType" }, {
@@ -28,5 +35,6 @@ return {
         vim.keymap.set('n', '<leader>ll', '<Cmd>VimtexCompile<CR>', { silent = true })
         vim.keymap.set('n', '<leader>lc', '<Cmd>VimtexClean<CR>', { silent = true })
         vim.keymap.set('n', '<leader>lv', '<Cmd>VimtexView<CR>', { silent = false })
+        vim.keymap.set('n', '<leader>lt', '<Cmd>VimtexTocToggle<CR>', { silent = false })
     end,
 }
